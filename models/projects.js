326f0e0e3d726a0be5540933752dea795492b338
John Bretherton <270022735@bun2-206-24.acg.ac.nz>
2019-09-05 09:19:25 +1200

    added project schema

--- models/projects.js
+++ models/projects.js
@@ -1,12 +1,13 @@
-// This creates a new schema (table) to contain classwork projects
-// const mongoose = require('mongoose');
-//
-// const projectSchema = new mongoose.Schema({
-//     _id: mongoose.Schema.Types.ObjectId,
-//     projectName: String,
-//     imageURL: String,
-//     author: String,
-//     websiteURL: String
-// });
-//
-// module.exports = mongoose.model('User', projectSchema);
+// This creates a new schema (table) to contain our classwork projects
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    author_id: String,
+    project_name: String,
+    author_name: String,
+    image_URL: String,
+    website_URL: String
+});
+
+module.exports = mongoose.model('Project', projectSchema);
