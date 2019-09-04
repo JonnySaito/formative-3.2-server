1effe29674bd18c509abc451a707bf7eb7629238
Andy Ball <932400399@bun2-206-11.local>
2019-09-05 09:13:55 +1200

    connecting to db

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-// This node server holds our data.
-// Required node modules might be:
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-// const config = require('./config.json');
-// const Classwork = require('./models/classwork');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const config = require('./config.json');
+
+mongoose.connect(`mongodb+srv://${config.MONGO_USERNAME}:${config.MONGO_PASSWORD}@andycluster-f7t5s.mongodb.net/formative?retryWrites=true&w=majority`,  {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('we are connected to mongo db');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+app.get('/', function(req, res){
+    res.send('test');
+});
+
+app.listen(port, () => {
+    // console.clear();
+    console.log(`application is running on port ${port}`)
+});
