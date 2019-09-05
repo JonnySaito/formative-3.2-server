6bead828e78c04ddd4e1cd8a231d7498636aacef
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-09-05 17:02:05 +1200

    server error

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const config = require('./config.json');
+const Project = require('./models/projects');
+const Author = require('./models/authors');;
 
 mongoose.connect(`mongodb+srv://${config.MONGO_USERNAME}:${config.MONGO_PASSWORD}@andycluster-f7t5s.mongodb.net/formative?retryWrites=true&w=majority`,  {useNewUrlParser: true});
 
@@ -22,23 +24,91 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 
+//Home Route
 app.get('/', function(req, res){
-    res.send('test');
+    res.send('Welcome to our project');
 });
 
-app.post('/addProject', function(req, res){
-  const product = new Project({
-    projectId: new mongoose.Types.ObjectId(),
-    // authorId: req.body.author_id,						// we'll add validating author soon !
-    authorName: req.body.author_name,
-    projectName: req.body.project_nane,
-		screenshotURL: req.body.screenshot_URL
-  });
+//Add a new Project
+// app.post('/addProject', function(req, res){
+//     console.log(req.body);
+//     const project = new Project({
+//     _id: new mongoose.Types.ObjectId(),
+//     authorId: req.body.author_id,
+//     authorName: req.body.author_name,
+//     projectName: req.body.project_name,
+// 		screenshotURL: req.body.screenshot_URL
+//   });
+//
+// 	project.save().then(result => {
+//       res.send(result);
+//   }).catch(err => res.send(err));
+// });
+//
+// // Get all Projects
+// app.get('/allProducts', function(req, res){
+//     Project.find().then(result => {
+//         res.send(result);
+//     })
+// })
+//
+// //Get single Project based on ID
+// app.post('/project/:id', function(req, res){
+//   const id = req.params.id;
+//
+//   Project.findById(id, function(err, project) {
+//     if (project['authorId'] == req.body.author_name) {
+//       res.send(project)
+//     } else {
+//       res.send('401')
+//     }
+//   })
+//
+// });
+//
+// //Get single Project based on ID
+// app.patch('/project/:id', function(req, res){
+//     const id = req.params.id;
+//
+//     Project.findById(id, function(err, product) {
+//       if (project['authorId'] == req.body.author_name) {
+//         const newUser = {
+//             authorName: req.body.author_name,
+//             projectName: req.body.project_name
+//         };
+//         Project.updateOne({ _id : id }, newUser).then(result => {
+//             res.send(result);
+//         }).catch(err => res.send(err));
+//       } else {
+//         res.send('401, permission denied')
+//       }
+//     }).catch(err => res.send('cannot find a product with that id'));
+// })
+//
+// // Register Route
+// app.post('/Authors', function(req, res){
+//     User.findOne({ username: req.body.username }, function (err, checkUser) {
+//         if(checkUser){
+//             res.send('user already exists');
+//         } else {
+//             const hash = bcrypt.hashSync(req.body.password);
+//             const user = new User({
+//                 _id: new mongoose.Types.ObjectId(),
+//                 username: req.body.username,
+//                 email: req.body.email,
+//                 password: hash
+//             });
+//             user.save().then(result => {
+//                 res.send(result);
+//             }).catch(err => res.send(err));
+//         }
+//     });
+// })
 
-	product.save().then(result => {
-      res.send(result);
-  }).catch(err => res.send(err));
-});
+
+
+
+
 
 app.listen(port, () => {
     // console.clear();
